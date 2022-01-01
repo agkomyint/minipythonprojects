fd7be3a1fd8e54e398c82ea0b37c1f41bda51f0f
agkomint <51739766+agkomyint@users.noreply.github.com>
2022-01-01 22:11:24 +0630

    Update minscounter.py

--- minscounter.py
+++ minscounter.py
@@ -2,12 +2,12 @@ import time
 import sys
 
 
-input("enter minutes ")
-for remaining in range(10, 0, -1):
+gg=int(input("Number of minuteto set the timer"))
+for remaining in range(gg, 0, -1):
     sys.stdout.write("\r")
     sys.stdout.write("{:2d} minutes remaining.".format(remaining)) 
     sys.stdout.flush()
     time.sleep(60)
 
-sys.stdout.write("\rComplete!            \n")
+sys.stdout.write("\rFinished           \n")
 
